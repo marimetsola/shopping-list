1ab1dbe7cc752f5427e69579f1f6ef8d17a7f7e6
Lari Löfman <larwazor@gmail.com>
2020-06-18 21:08:16 +0300

    fix frontend list service

--- frontend/src/services/lists.tsx
+++ frontend/src/services/lists.tsx
@@ -20,7 +20,7 @@ const getListsByUser = async () => {
 
 const addList = async (name: string) => {
     const { data: addedList } = await axios.post<ItemList>(
-        `${apiBaseUrl}/lists`, { name }
+        `${apiBaseUrl}/lists`, { name }, config()
     );
 
     return addedList;
@@ -33,25 +33,25 @@ const editList = async (id: string, items: ItemType[]) => {
 };
 
 const deleteList = async (listID: string) => {
-    await axios.delete(`${apiBaseUrl}/lists/${listID}`);
+    await axios.delete(`${apiBaseUrl}/lists/${listID}`, config());
 };
 
 const addItem = async (listID: string, item: string) => {
     const addedItem = await axios.post<ItemType>(
-        `${apiBaseUrl}/lists/${listID}/add-item`, { name: item }
+        `${apiBaseUrl}/lists/${listID}/add-item`, { name: item }, config()
     );
     return addedItem;
 };
 
-const deleteItem = async (listID: string, itemID: string) => {
+const deleteItem = async (listID: string, itemId: string) => {
     await axios.delete<ItemList>(
-        `${apiBaseUrl}/lists/${listID}/delete-item`, { data: { itemID } }
+        `${apiBaseUrl}/lists/${listID}/delete-item/${itemId}`, config()
     );
 };
 
 const editItem = async (listID: string, item: ItemType) => {
     await axios.patch<ItemList>(
-        `${apiBaseUrl}/lists/${listID}/edit-item`, { item }
+        `${apiBaseUrl}/lists/${listID}/edit-item`, { item }, config()
     );
 };
 
